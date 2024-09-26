c1eafb22b7f58343cc8af5c075750e7c6b2b7841
hyeon-1122 <leehaeyeon1122@gmail.com>
2024-09-26 22:41:16 +0900

    third

--- src/Hello_first.java
+++ src/Hello_first.java
@@ -2,6 +2,6 @@ public class Hello_first {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
         System.out.println("It's my first vs code");
-        System.out.println("first");
+        System.out.println("Soooo difficult");
     }
 }
